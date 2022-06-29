ba1ac74fcd8c6e40a42f0ef0bb9ef2dd235b1585
yamabe009 <yamabe.kinoko@gmail.com>
2022-06-30 08:06:57 +0900

    抽象的すぎる変数名によって不具合が発生していたので、具体的な変数名に変更

--- cafe.rb
+++ cafe.rb
@@ -25,10 +25,10 @@ def take_order(menus)
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+order_drink = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+order_food = take_order(FOODS)
 
-total = DRINKS[order1][:price] + FOODS[order2][:price]
+total = DRINKS[order_drink][:price] + FOODS[order_food][:price]
 puts "お会計は#{total}円になります。ありがとうございました！"
